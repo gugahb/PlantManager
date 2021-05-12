285f1f9716004449cb7c398ed910d3b105f04e2c
Gustavo Borges <gustavohbcn@gmail.com>
2021-05-11 21:34:30 -0300

    Aula 5

--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -4,22 +4,48 @@ import {
     View, 
     Text, 
     Image,
-    FlatList
+    FlatList,
+    Alert
 } from 'react-native';
 import { Header } from '../components/Header';
 import colors from '../styles/colors';
 import waterdrop from '../assets/waterdrop.png';
-import { PlantProps, loadPlant } from '../libs/storage';
+import { PlantProps, loadPlant, removePlant } from '../libs/storage';
 import { formatDistance } from 'date-fns';
 import { pt } from 'date-fns/locale'
 import fonts from '../styles/fonts';
 import { PlantCardSecondary } from '../components/PlantCardSecondary';
+import { Load } from '../components/Load';
 
 
 export function MyPlants(){
     const [myPlants, setMyPlants] = useState<PlantProps[]>([]);
     const [loading, setLoading ] = useState(true);
     const [nextWatered, setNextWatered] = useState<string>();
+
+    function handleRemove(plant: PlantProps){
+        Alert.alert('Remover', `Deseja remover a ${plant.name} ?`,[
+            {
+                text: 'Não 🙏',
+                style: 'cancel'
+            },
+            {
+                text: 'Sim 😃',
+                onPress: async () => {
+                    try {
+                            await removePlant(plant.id);
+                            setMyPlants((oldData) => 
+                                oldData.filter((item) => item.id != plant.id)
+                            );
+
+                    } catch (error) {
+                        Alert.alert('Não foi possível remover');
+
+                    }
+                }
+            }
+        ])
+    }
 
     useEffect(() => {
         async function loadStorageData() {
@@ -40,7 +66,10 @@ export function MyPlants(){
         }
 
         loadStorageData();
-    })
+    },[])
+
+    if(loading)
+        return <Load />
 
     return (
         <View style={styles.container}>
@@ -65,7 +94,10 @@ export function MyPlants(){
                     data={myPlants}
                     keyExtractor={(item) => String(item.id)}
                     renderItem={({item}) => (
-                        <PlantCardSecondary data={item} />
+                        <PlantCardSecondary 
+                            data={item} 
+                            handleRemove={() => {handleRemove(item)}}
+                        />
                     )}
                     showsVerticalScrollIndicator={false}
                     contentContainerStyle={{ flex: 1 }}
